c75756bdc638211929616ec2e894b765084b525e
Pac4 <amanoskull42@gmail.com>
2023-08-11 16:18:18 +0100

    Saving "progress"

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: paugonca <paugonca@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 11:01:34 by paugonca          #+#    #+#             */
-/*   Updated: 2023/08/11 15:06:44 by paugonca         ###   ########.fr       */
+/*   Updated: 2023/08/11 16:14:59 by paugonca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,13 @@ int g_exitstts = EXIT_SUCCESS;
 int	main(int ac, char **av, char **envp)
 {
 	t_list	*env;
-	t_tree	*root;
+//	t_tree	*root;
 	(void) ac;
 	(void) av;
 
 	get_cur_env(&env, envp);
-	root->token = "ls";
-	xqt(&root, &env, 1);
+//	root->token = "ls";
+//	xqt(&root, &env, 1);
+	print_mtx(lst2mtx(env));
 	return (g_exitstts);
 }
