a0600683fbb050254eb847382ae79d5f7b2902e0
Pedrocasss <pedroacnesisma13@gmail.com>
2023-10-07 00:11:29 +0100

    exit without segfault but not perfect

--- src/builtins/ft_exit.c
+++ src/builtins/ft_exit.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 
 #include "../minishell.h"
-
+/*
 static bool	is_num(char *str)
 {
 	int		i;
@@ -32,9 +32,31 @@ static bool	is_num(char *str)
 	if (!str[i] && check)
 		return (true);
 	return (false);
+}*/
+
+static int	is_nbr(char *str)
+{
+	int	i;
+	int	nbr;
+
+	nbr = 0;
+	i = 0;
+	while (str[i] && (str[i] == ' ' || str[i] == '\t'))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	while (str[i] && str[i] >= '0' && str[i] <= '9')
+	{
+		if (!nbr)
+			nbr = 1;
+		i++;
+	}
+	if (!str[i] && nbr)
+		return (1);
+	return (0);
 }
 
-static int	quit(char **args, bool isnum)
+static int	quit(char **args, int isnum)
 {
 	if (isnum && args[2])
 	{
@@ -46,17 +68,17 @@ static int	quit(char **args, bool isnum)
 	}
 	g_stts = 2;
 	ft_putstr_fd("exit\n", STDERR_FILENO);
-	ft_putstr_fd(SHELL, STDERR_FILENO);
+	/*ft_putstr_fd(SHELL, STDERR_FILENO);
 	ft_putstr_fd(": exit: ", STDERR_FILENO);
 	ft_putstr_fd(args[1], STDERR_FILENO);
-	ft_putstr_fd(":	numeric argument required\n", STDERR_FILENO);
+	ft_putstr_fd(":	numeric argument required\n", STDERR_FILENO);*/
 	free_mtx(args);
 	rl_clear_history();
-	exit(g_stts);
+	exit(2);
 	return (-42);
 }
 
-int	ft_exit(char **args)
+/*int	ft_exit(char **args)
 {
 	bool	isnum;
 
@@ -78,4 +100,28 @@ int	ft_exit(char **args)
 		exit(g_stts);
 	}
 	return (quit(args, isnum));
+}*/
+
+int	ft_exit(char **args)
+{
+	int	nbr;
+
+	nbr = 0;
+	if (args[1] && is_nbr(args[1]))
+		nbr = 1;
+	if ((nbr && mtx_len(args) == 2) || mtx_len(args) == 1)
+	{
+		ft_putstr_fd("exittest\n", STDOUT_FILENO);
+		if (nbr)
+		{
+			g_stts = (unsigned int) ft_atoi(args[1]);
+			free_mtx(args);
+			rl_clear_history();
+			exit(g_stts);
+		}
+		free_mtx(args);
+		rl_clear_history();
+		exit(g_stts);
+	}
+	return (quit(args, nbr));
 }
