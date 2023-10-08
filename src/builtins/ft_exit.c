83387b7aebc39324c466e293ec5feb61e3dae040
Pedrocasss <pedroacnesisma13@gmail.com>
2023-10-08 16:48:18 +0100

    exit done, but need to check numeric arg

--- src/builtins/ft_exit.c
+++ src/builtins/ft_exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_exit.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: paula <paula@student.42.fr>                +#+  +:+       +#+        */
+/*   By: psoares- <psoares-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 14:43:43 by jferreir          #+#    #+#             */
-/*   Updated: 2023/10/04 11:19:43 by paugonca         ###   ########.fr       */
+/*   Updated: 2023/10/08 16:37:20 by psoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,20 +58,20 @@ static int	is_nbr(char *str)
 
 static int	quit(char **args, int isnum)
 {
-	if (isnum && args[2])
+	if (isnum == 1 && args[2])
 	{
 		ft_putstr_fd(SHELL, STDERR_FILENO);
-		ft_putstr_fd(": exit: too many arguments", STDERR_FILENO);
+		ft_putstr_fd(": exit: too many arguments\n", STDERR_FILENO);
 		free_mtx(args);
 		g_stts = EXIT_FAILURE;
 		return (-1);
 	}
 	g_stts = 2;
 	ft_putstr_fd("exit\n", STDERR_FILENO);
-	/*ft_putstr_fd(SHELL, STDERR_FILENO);
+	ft_putstr_fd(SHELL, STDERR_FILENO);
 	ft_putstr_fd(": exit: ", STDERR_FILENO);
 	ft_putstr_fd(args[1], STDERR_FILENO);
-	ft_putstr_fd(":	numeric argument required\n", STDERR_FILENO);*/
+	ft_putstr_fd(":	numeric argument required\n", STDERR_FILENO);
 	free_mtx(args);
 	rl_clear_history();
 	exit(2);
@@ -107,12 +107,12 @@ int	ft_exit(char **args)
 	int	nbr;
 
 	nbr = 0;
-	if (args[1] && is_nbr(args[1]))
+	if ((args[1] && is_nbr(args[1])) || args[0])
 		nbr = 1;
-	if ((nbr && mtx_len(args) == 2) || mtx_len(args) == 1)
+	if ((nbr == 1 && mtx_len(args) == 2) || mtx_len(args) == 1)
 	{
-		ft_putstr_fd("exittest\n", STDOUT_FILENO);
-		if (nbr)
+		ft_putstr_fd("exit\n", STDOUT_FILENO);
+		if (nbr == 1)
 		{
 			g_stts = (unsigned int) ft_atoi(args[1]);
 			free_mtx(args);
