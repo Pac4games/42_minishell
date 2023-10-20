ab4b880ac0935c585758f7891de08f8b1201bc2f
Pac4games <paulo.nicolau7@gmail.com>
2023-10-20 13:50:41 +0100

    Small patch to ft_echo

--- src/builtins/ft_echo.c
+++ src/builtins/ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: paugonca <paugonca@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 14:40:06 by paugonca          #+#    #+#             */
-/*   Updated: 2023/10/16 14:40:07 by paugonca         ###   ########.fr       */
+/*   Updated: 2023/10/20 13:49:51 by paugonca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ int	ft_echo(char **args, int fd)
 			ft_putstr_fd(" ", fd);
 		i++;
 	}
-	if (!(mtx_len(args) > 1 && is_flag(args[1])))
+	if (!(mtx_len(args) > 1 && is_flag(args[1])) || !args[1][0])
 		ft_putchar_fd('\n', fd);
 	free_mtx(args);
 	*exit_stts() = 0;
